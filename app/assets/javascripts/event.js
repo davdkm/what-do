c7cac9633f63cb57132d3a1420ba9752ca9123ee
davdkm <davd.km@gmail.com>
2016-06-21 23:16:24 -0700

    moved event detail js to asset pipeline

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -8,3 +8,16 @@ var attachListeners = function() {
     eventDetail(event);
   })
 }
+
+
+var eventDetail = function() {
+  $(".js-more").on("click", function() {
+    var id = $(this).data("id");
+    $.get("/events/" + id + ".json", function(event) {
+      var startTime = "<p><strong>Begins:</strong> " + event.readable_start_time + "</p>";
+      var endTime = "<p><strong>Ends: </strong> " + event.readable_end_time + "</p>";
+      var description = "<p><strong>Description:</strong> " + event.description + "</p>";
+      $("#event-" + id + "-detail").append(startTime + startTime + endTime + description);
+    });
+  });
+}
