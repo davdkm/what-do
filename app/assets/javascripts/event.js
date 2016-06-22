168f030a81c47a2a0bcfd9c194d713c7d3cab15e
davdkm <davd.km@gmail.com>
2016-06-22 14:53:32 -0700

    added comment serializer

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
-  attachListeners();
   console.log('this is loading');
+  attachListeners();
 });
 
+
 var attachListeners = function() {
-  $(".js-more").on("click", function(event) {
-    eventDetail(event);
-  })
+  eventDetail();
 }
 
-
 var eventDetail = function() {
+  console.log('eventDetail hit');
   $(".js-more").on("click", function() {
     var id = $(this).data("id");
     $.get("/events/" + id + ".json", function(event) {
-      var startTime = "<p><strong>Begins:</strong> " + event.readable_start_time + "</p>";
-      var endTime = "<p><strong>Ends: </strong> " + event.readable_end_time + "</p>";
-      var description = "<p><strong>Description:</strong> " + event.description + "</p>";
-      $("#event-" + id + "-detail").append(startTime + startTime + endTime + description);
+      var dom = '';
+      dom += "<p><strong>Begins:</strong> " + event.readable_start_time + "</p>";
+      dom += "<p><strong>Ends: </strong> " + event.readable_end_time + "</p>";
+      dom += "<p><strong>Description:</strong> " + event.description + "</p>";
+      $("#event-" + id + "-detail").html(dom);
     });
+    $(this).remove();
   });
 }
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :location, :readable_start_time, :readable_end_time, :description
-  belongs_to :organizer
+  has_one :organizer
+  has_many :users
+  has_many :comments
 end
