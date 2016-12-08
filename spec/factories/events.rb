8959d0d4e24c808b4e76156673d480a7f2a43e28
davdkm <davd.km@gmail.com>
2016-12-07 17:04:58 -0800

    Create events from factory girl

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,14 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :event do
-    # name "MyString"
-    # Event.first
+    sequence(:name) {|n| "Test Event#{n}"}
+    description "A Temporary event to test with."
+    location "Pershing Square"
+    time_zone "Pacific Time (US & Canada)"
+    start_time Faker::Time.between(DateTime.now + 1, DateTime.now + 2)
+    end_time Faker::Time.between(DateTime.now + 2, DateTime.now + 3)
+    association :organizer, factory: :user
+    # association :tags, factory: :tags
   end
 end
