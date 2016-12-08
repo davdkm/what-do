1aa41ff5bb41ca5094699dcac227594b13b86c97
davdkm <davd.km@gmail.com>
2016-12-07 17:05:40 -0800

    Tests basic user attr and associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,47 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before(:each) do
-    @user = User.first
+    @user = build_stubbed(:user, name: 'Nathan', email: 'nathan@email.com')
   end
 
-  it "has a name" do
-    @user.name = "Nathan"
+  it 'has a name' do
     expect(@user.name).to eq('Nathan')
   end
 
-  it "can have many events to go to" do
-    @user.attending_events.create
-    @user.attending_events.create
-
-    expect(@user.attending_events.count).to eq(2)
+  it 'has an email address' do
+    expect(@user.email).to eq('nathan@email.com')
   end
 
-  it "has many comments" do
-    @user.comments << @user.comments.create
-    expect(@user.comments.first).to be_a(Comment)
+  describe '#Associations' do
+    before(:each) do
+      @event = create(:event)
+    end
+
+    it 'can organize an event' do
+      @event.organizer = @user
+
+      expect(@event.organizer).to be_kind_of(User)
+      expect(@event.organizer.name).to eq('Nathan')
+    end
+
+    it 'can write a comment' do
+      @comment = create(:comment, user: @user, event: @event, content: 'SWAG')
+
+      expect(@user.comments.size).to eq(1)
+      expect(@user.comments.first).to be_kind_of(Comment)
+      expect(@user.comments.first.content).to eq('SWAG')
+    end
+
+    it 'can attend an event' do
+      @new_user = create(:user, name: 'Brian', email: 'brian@email.com')
+      @schedule = Schedule.create(user: @new_user, event: @event)
+
+      expect(@event.users.size).to eq(1)
+      expect(@event.users.first.name).to eq('Brian')
+      expect(@new_user.events.first).to be_kind_of(Event)
+    end
   end
+
+
 
 end
